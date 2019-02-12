e065cf1ce40d4bacd77eff2fb6bcdb52d494895f
bzt <bztemail@gmail.com>
2019-02-13 00:13:22 +0100

    ANSI C comments

--- bootboot.h
+++ bootboot.h
@@ -37,32 +37,32 @@ extern "C" {
 
 #define BOOTBOOT_MAGIC "BOOT"
 
-// minimum protocol level:
-//  hardcoded kernel name, static kernel memory addresses
+/* minimum protocol level:
+ *  hardcoded kernel name, static kernel memory addresses */
 #define PROTOCOL_MINIMAL 0
-// static protocol level:
-//  kernel name parsed from environment, static kernel memory addresses
+/* static protocol level:
+ *  kernel name parsed from environment, static kernel memory addresses */
 #define PROTOCOL_STATIC  1
-// dynamic protocol level:
-//  kernel name parsed, kernel memory addresses from ELF or PE symbols
+/* dynamic protocol level:
+ *  kernel name parsed, kernel memory addresses from ELF or PE symbols */
 #define PROTOCOL_DYNAMIC 2
-// big-endian flag
+/* big-endian flag */
 #define PROTOCOL_BIGENDIAN 0x80
 
-// loader types, just informational
+/* loader types, just informational */
 #define LOADER_BIOS (0<<2)
 #define LOADER_UEFI (1<<2)
 #define LOADER_RPI  (2<<2)
 
-// framebuffer pixel format, only 32 bits supported
+/* framebuffer pixel format, only 32 bits supported */
 #define FB_ARGB   0
 #define FB_RGBA   1
 #define FB_ABGR   2
 #define FB_BGRA   3
 
-// mmap entry, type is stored in least significant tetrad (half byte) of size
-// this means size described in 16 byte units (not a problem, most modern
-// firmware report memory in pages, 4096 byte units anyway).
+/* mmap entry, type is stored in least significant tetrad (half byte) of size
+ * this means size described in 16 byte units (not a problem, most modern
+ * firmware report memory in pages, 4096 byte units anyway). */
 typedef struct {
   uint64_t   ptr;
   uint64_t   size;
@@ -72,33 +72,33 @@ typedef struct {
 #define MMapEnt_Type(a) (a->size & 0xF)
 #define MMapEnt_IsFree(a) ((a->size&0xF)==1||(a->size&0xF)==2)
 
-#define MMAP_USED     0   // don't use. Reserved or unknown regions
-#define MMAP_FREE     1   // usable memory
-#define MMAP_ACPIFREE 2   // free to use after acpi tables are parsed
-#define MMAP_ACPINVS  3   // don't use. Acpi non-volatile
-#define MMAP_MMIO     4   // memory mapped IO region
+#define MMAP_USED     0   /* don't use. Reserved or unknown regions */
+#define MMAP_FREE     1   /* usable memory */
+#define MMAP_ACPIFREE 2   /* free to use after acpi tables are parsed */
+#define MMAP_ACPINVS  3   /* don't use. Acpi non-volatile */
+#define MMAP_MMIO     4   /* memory mapped IO region */
 
-#define INITRD_MAXSIZE 16 //Mb
+#define INITRD_MAXSIZE 16 /* Mb */
 
 typedef struct {
-  // first 64 bytes is platform independent
-  uint8_t    magic[4];    // 'BOOT' magic
-  uint32_t   size;        // length of bootboot structure, minimum 128
-  uint8_t    protocol;    // 1, static addresses, see PROTOCOL_* and LOADER_* above
-  uint8_t    fb_type;     // framebuffer type, see FB_* above
-  uint16_t   numcores;    // number of processor cores
-  uint16_t   bspid;       // Bootsrap processor ID (Local APIC Id on x86_64)
-  int16_t    timezone;    // in minutes -1440..1440
-  uint8_t    datetime[8]; // in BCD yyyymmddhhiiss UTC (independent to timezone)
-  uint64_t   initrd_ptr;  // ramdisk image position and size
+  /* first 64 bytes is platform independent */
+  uint8_t    magic[4];    /* 'BOOT' magic */
+  uint32_t   size;        /* length of bootboot structure, minimum 128 */
+  uint8_t    protocol;    /* 1, static addresses, see PROTOCOL_* and LOADER_* above */
+  uint8_t    fb_type;     /* framebuffer type, see FB_* above */
+  uint16_t   numcores;    /* number of processor cores */
+  uint16_t   bspid;       /* Bootsrap processor ID (Local APIC Id on x86_64) */
+  int16_t    timezone;    /* in minutes -1440..1440 */
+  uint8_t    datetime[8]; /* in BCD yyyymmddhhiiss UTC (independent to timezone) */
+  uint64_t   initrd_ptr;  /* ramdisk image position and size */
   uint64_t   initrd_size;
-  uint8_t    *fb_ptr;     // framebuffer pointer and dimensions
+  uint8_t    *fb_ptr;     /* framebuffer pointer and dimensions */
   uint32_t   fb_size;
   uint32_t   fb_width;
   uint32_t   fb_height;
   uint32_t   fb_scanline;
 
-  // the rest (64 bytes) is platform specific
+  /* the rest (64 bytes) is platform specific */
   union {
     struct {
       uint64_t acpi_ptr;
