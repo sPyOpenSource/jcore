7329e9589f3ffbb86aec01f54834fe959a9e0f2e
Xuyi Wang <xuyi@tetra.nl>
2019-11-12 13:29:09 +0100

    v1.12 XW 12-11-2019 fixed: run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #building the iso file
 #mkdir -p isodir/boot/grub
-make jxcore
+#make jxcore
 strip jxcore ; cat jxcore | gzip -9 -c > isodir/jxcore
 cd isodir/code
 zip -0 uncompressed.zip *.jll
@@ -8,6 +8,6 @@ cat uncompressed.zip | gzip --fast -c > ../code.zip
 cd ../..
 grub-mkrescue -o MyOS.iso isodir
 (killall VirtualBox && sleep 1) || true
-VirtualBox --startvm 'JavaOS' &
+VBoxManage startvm 'JavaOS' &
 #run it in qemu
 #qemu-system-x86_64 -cdrom MyOS.iso
