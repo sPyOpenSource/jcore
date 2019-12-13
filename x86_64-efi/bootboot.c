84415afd2f126dd4c2a48b4f4c88af5121d3a51a
bzt <bztemail@gmail.com>
2019-12-13 12:20:45 +0100

    Disabled FS/Z support by default

--- x86_64-efi/bootboot.c
+++ x86_64-efi/bootboot.c
@@ -49,7 +49,7 @@
 #include "../bootboot.h"
 #include "tinf.h"
 // comment out this include if you don't want FS/Z support
-#include "../../osZ/include/osZ/fsZ.h"
+//#include "../../osZ/include/osZ/fsZ.h"
 
 /*** ELF64 defines and structs ***/
 #define ELFMAG      "\177ELF"
