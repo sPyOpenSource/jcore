107524ac6b2f2c78457485b56656095f962b1806
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2023-04-23 14:56:44 -0400

    Update go

---
+++ mykernel/go/bootboot/bootboot_amd64.go
@@ -0,0 +1,12 @@
+package bootboot
+
+type arch struct {
+	AcpiPtr uint64
+	SmbiPtr uint64
+	EfiPtr  uint64
+	MpPtr   uint64
+	_       uint64
+	_       uint64
+	_       uint64
+	_       uint64
+}
---
+++ mykernel/go/bootboot/bootboot_arm64.go
@@ -0,0 +1,12 @@
+package bootboot
+
+type arch struct {
+	AcpiPtr uint64
+	MmioPtr uint64
+	EfiPtr  uint64
+	_       uint64
+	_       uint64
+	_       uint64
+	_       uint64
+	_       uint64
+}
