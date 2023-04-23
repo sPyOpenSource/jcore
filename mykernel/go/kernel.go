25b6346b95ea44fd6c65f6f50d6246c062860332
bzt <bztemail@gmail.com>
2023-04-23 19:43:04 +0000

    Added copyright

--- mykernel/go/kernel.go
+++ mykernel/go/kernel.go
@@ -2,7 +2,8 @@
  * mykernel/go/kernel.go
  *
  * Copyright (C) 2017 - 2021 bzt (bztsrc@gitlab)
- *
+ * Copyright (C) 2023 TotallyGamerJet (rewrite for standard go toolchain)
+
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
