b88e9f82cf25c772ec745d72d6ad723a16eda7e6
Binary Craft <keep.it.sns@gmail.com>
2023-10-08 23:15:08 +0800

    update zig example to latest zig

--- mykernel/zig/build.zig
+++ mykernel/zig/build.zig
@@ -1,18 +1,29 @@
 const std = @import("std");
+const NativeTargetInfo = std.zig.system.NativeTargetInfo;
 
-pub fn build(b: *std.build.Builder) void {
-    var target = std.zig.CrossTarget{
-        .os_tag = .freestanding,
-        .cpu_arch = .x86_64,
-        .abi = .none,
+pub fn build(b: *std.build.Builder) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const target_info = try NativeTargetInfo.detect(target);
+    const name = switch (target_info.target.cpu.arch) {
+        inline else => |arch| "mykernel." ++ @tagName(arch) ++ ".elf",
     };
-
-    const mode = b.standardReleaseOptions();
-    const kernel = b.addExecutable("mykernel.x86_64.elf", "src/main.zig");
+    const kernel = b.addExecutable(.{
+        .name = name,
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     kernel.setLinkerScriptPath(.{ .path = "src/link.ld" });
-    kernel.code_model = .kernel;
-    kernel.setTarget(target);
-    kernel.setBuildMode(mode);
-    kernel.install();
+    kernel.code_model = switch (target_info.target.cpu.arch) {
+        .x86_64 => .kernel,
+        .riscv64 => .medium,
+        .aarch64 => .large,
+        else => |arch| {
+            std.debug.print("unsupported arch: {}", .{arch});
+            std.os.exit(1);
+        },
+    };
     kernel.strip = true;
+    b.installArtifact(kernel);
 }
