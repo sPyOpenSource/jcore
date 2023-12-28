858b106e4c30f11b59043fbb28c421308dc1d8b3
X. Wang <xuyi@tetra.nl>
2023-12-28 16:42:55 +0100

    save

--- lib/elf-loader/src/lib.rs
+++ lib/elf-loader/src/lib.rs
@@ -1,11 +1,11 @@
 #![no_std]
 
-use elf_rs::{ElfFile, ProgramHeaderWrapper, ProgramType};
+use elf_rs::{ElfFile, ProgramHeaderIter, ProgramType, ProgramHeaderEntry};
 use core::iter::Iterator;
 
 pub trait ElfLoader {
-    fn allocate<'a>(&mut self, load_headers: &mut dyn Iterator<Item=ProgramHeaderWrapper>) -> Result<(), &'static str>;
-    fn load(&mut self, program_header: ProgramHeaderWrapper) -> Result<(), &'static str>;
+    fn allocate<'a>(&mut self, load_headers: &mut dyn Iterator<Item=ProgramHeaderEntry>) -> Result<(), &'static str>;
+    fn load(&mut self, program_header: ProgramHeaderEntry) -> Result<(), &'static str>;
     // fn relocate(&mut self, _entry: &Rela<P64>) -> Result<(), &'static str>
     fn load_elf(&mut self, elf: &dyn ElfFile) -> Result<(), &'static str> {
         let mut loadable_headers = elf.program_header_iter().filter(|ph| ph.ph_type() == ProgramType::LOAD);
