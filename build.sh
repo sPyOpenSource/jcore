00e68b72e64969ca5ed95e7e0c010fd5ee340fdb
X. Wang <xuyi@tetra.nl>
2024-01-04 21:41:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rustup target add aarch64-unknown-linux-gnu
 rustup override set nightly
 rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
-sudo apt-get install gcc-aarch64-linux-gnu
+sudo apt-get install aarch64-none-elf binutils
 cd kernel
 make
