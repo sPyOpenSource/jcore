d4c44c473ba31fe883d9e358a340d6569dfffc34
X. Wang <xuyi@tetra.nl>
2024-01-08 10:44:38 +0100

    save

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rustup override set nightly
 rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+rustup component add clippy --toolchain nightly-x86_64-unknown-linux-gnu
 cargo build
