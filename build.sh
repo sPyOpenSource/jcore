853b1e39a89fa7b34ea89d71c8249244c46ab701
X. Wang <xuyi@tetra.nl>
2024-01-21 19:06:49 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rustup target add aarch64-unknown-linux-gnu
 rustup override set nightly
 rustup component add rust-src
 sudo apt-get install gcc-aarch64-linux-gnu binutils
-cd ricsv
+cd rics_v
 cargo build
