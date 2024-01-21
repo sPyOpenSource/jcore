7e07f0fc18e33d0d6d586b5e725876f4b91835cf
X. Wang <xuyi@tetra.nl>
2024-01-21 16:42:05 +0100

    save

--- src/main.c
+++ src/main.c
@@ -27,19 +27,19 @@ void mmio_init()
         default:    board = "????"; MMIO_BASE = 0x20000000; break;
     }
 }
- 
+
 // Memory-Mapped I/O output
 static inline void mmio_write(uint32_t reg, uint32_t data)
 {
 	*(volatile uint32_t*)(MMIO_BASE + reg) = data;
 }
- 
+
 // Memory-Mapped I/O input
 static inline uint32_t mmio_read(uint32_t reg)
 {
 	return *(volatile uint32_t*)(MMIO_BASE + reg);
 }
- 
+
 // Loop <delay> times in a way that the compiler won't optimize away
 static inline void delay(int32_t count)
 {
@@ -147,21 +147,21 @@ void full_uart_init(int raspi)
 	// Enable UART0, receive & transfer part of UART.
 	mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
 }
- 
+
 void uart_putc(unsigned char c)
 {
 	// Wait for UART to become ready to transmit.
 	while ( mmio_read(UART0_FR) & (1 << 5) ) { }
 	mmio_write(UART0_DR, c);
 }
- 
+
 unsigned char uart_getc()
 {
     // Wait for UART to have received something.
     while ( mmio_read(UART0_FR) & (1 << 4) ) { }
     return mmio_read(UART0_DR);
 }
- 
+
 void uart_puts(const char* str)
 {
 	for (size_t i = 0; str[i] != '\0'; i ++)
