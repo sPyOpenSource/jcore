fab534a799d83b2805234f4b87e52d02ca479d22
X. Wang <xuyi@tetra.nl>
2024-01-21 03:30:14 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -104,7 +104,7 @@ volatile unsigned int  __attribute__((aligned(16))) mbox[9] = {
     9*4, 0, 0x38002, 12, 8, 2, 3000000, 0 ,0
 };
 
-void uart_init(int raspi)
+void full_uart_init(int raspi)
 {
 	mmio_init(raspi);
  
