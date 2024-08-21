6a78d390c0905c77e7d18e573ed19cc8592f0238
X. Wang <xuyi@tetra.nl>
2024-08-21 06:51:01 +0200

    Update zero_Memory_new.c

--- src/interface/zero_Memory_new.c
+++ src/interface/zero_Memory_new.c
@@ -40,6 +40,27 @@ jshort memory_get16(MemoryProxy * self, jint where);
 void memory_set32(MemoryProxy * self, jint where, jint what);
 jint memory_get32(MemoryProxy * self, jint where);
 void memory_fill16(MemoryProxy * self, jshort what, jint where, jint length);
+void memory_fill32(MemoryProxy * self, jint what, jint where, jint length);
+void memory_clear(MemoryProxy * self);
+void memory_move(MemoryProxy * self, jint __dst, jint __src, jint count);
+void memory_copy(MemoryProxy * self, jint from, jint to, jint len);
+void memory_copyToByteArray(MemoryProxy * self, ArrayDesc * bytes, jint array_offset, jint mem_offset, jint len);
+void memory_copyFromByteArray(MemoryProxy * self, ArrayDesc * bytes, jint array_offset, jint mem_offset, jint len);
+void memory_copyToMemory(MemoryProxy * self, MemoryProxy * dst, jint srcOffset, jint dstOffset, jint len);
+void memory_copyFromMemory(MemoryProxy * self, MemoryProxy * src, jint srcOffset, jint dstOffset, jint len);
+jint memory_getLittleEndian32(MemoryProxy * self, jint offset);
+void memory_setLittleEndian32(MemoryProxy * self, jint offset, jint value);
+jshort memory_getLittleEndian16(MemoryProxy * self, jint offset);
+void memory_setLittleEndian16(MemoryProxy * self, jint offset, jshort value);
+jint memory_getBigEndian32(MemoryProxy * self, jint offset);
+void memory_setBigEndian32(MemoryProxy * self, jint offset, jint value);
+jshort memory_getBigEndian16(MemoryProxy * self, jint offset);
+void memory_setBigEndian16(MemoryProxy * self, jint offset, jshort value);
+jint memory_size(MemoryProxy * self);
+jint memory_getStartAddress(MemoryProxy * self);
+u4_t memory_sizeof_proxy();
+ClassDesc *mem_getDeviceMemoryClass();
+u4_t memory_getMem(MemoryProxy * self);
 
 
 /*******************************************
