9ec832d2be2411d571f9ed834ecbe5c974eefacf
X. Wang <xuyi@tetra.nl>
2026-07-05 10:40:04 +0200

    save

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -457,9 +457,10 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t multiboot_m
 
     while(1)
     {
-    #ifdef GRAPHICSMODE
+    /*#ifdef GRAPHICSMODE
         desktop.Draw(&vga);
-    #endif
+    #endif*/
+        
+        __asm__ volatile("hlt"); 
     }
-
 }
